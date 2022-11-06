47b0e906a038971b19fb83dda1ae1942deac551f
Zian Kang <michael040624@gmail.com>
2022-11-06 10:49:47 -0500

    homework 7 completed

--- Python/hw7/hw7.py
+++ Python/hw7/hw7.py
@@ -1,69 +1,15 @@
 #Zian Kang, homework 7: Loops and Booleans
 
-'''
 def get_input():
 	while True:
-		number = input("Please enter a positive integer: ")
-		num = list(number)
-		for i in range(len(num)):
-			if ord(num[i]) < 48 or ord(num[i]) > 57:
-				is_int = False
-				if num[0] == "-":
-					if ord(num[i]) < 48 or ord(num[i]) > 57:
-						is_int = False
-						break
-					else:
-						is_int = True
-						break
-				i += 1
-				if is_int == True:
-					print("The integer must be positive.")
-				else:
-					print("Bad input!")
-			else:
-				n = int(number)
+		try:
+			n = int(input("Please enter a positive integer: "))
+			if n > 0:
 				return n
-'''
-
-def get_input():
-	while True:
-		b_in = False
-		n_int = False
-		p_int = False
-		number = input("Please enter a positive integer: ")
-		num = list(number)
-		for i in range(len(num)):
-			if num[0] == "-":
-				for m in range(len(num) - 1):
-					if (ord(num[m + 1]) < 48 or ord(num[m + 1]) > 57):
-						b_in = True
-						break
-					else:
-						n_int = True
-						continue
-			else:
-				if ord(num[i]) >= 48 and ord(num[i]) <= 57:
-					p_int = True
-					continue
-				else:
-					b_in = True
-					break
-			if b_in == True:
-				break
-		if n_int == True:
-			if b_in == True:
-				print("Bad input!")
 			else:
 				print("The integer must be positive.")
-		elif p_int == True:
-			if b_in == True:
-				print("Bad input!")
-			else:
-				break
-		elif n_int == False and p_int == False and b_in == True:
+		except:
 			print("Bad input!")
-	n = int(number)
-	return n
 
 def lagrange(n):
 	m = int(pow(n, 0.5)) + 1
